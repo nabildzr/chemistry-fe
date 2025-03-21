79f85fdec892719c50cbb60bcb29109e7e2e97a4
nabildzr <muhammadnbillz@gmail.com>
2025-03-21 13:15:40 +0700

    remove unused assets and add dashboard page component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,101 +1,50 @@
 "use client";
 
-import React, { useState } from "react";
-import Image from "next/image";
-import { useRouter } from "next/navigation";// Import the Sidebar component
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
-const Navbar: React.FC = () => {
-  const router = useRouter();
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+const NavbarLayout = ({ children }: { children: React.ReactNode }) => {
 
-  const handleNavigation = (path: string) => {
-    router.push(path);
-  };
+  const pathname = usePathname();
+  const [showNav, setShowNav] = useState<Boolean>(false);
 
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
+  useEffect(() => {
+      const hiddenNavPaths = {
+        signin: '/signin',
+        signup: '/signup',
+      };
+      
+      if(Object.values(hiddenNavPaths).includes(pathname)) {
+        setShowNav(false);
+      } else {
+        setShowNav(true);
+      }
+  }, [pathname])
 
   return (
-    <>
-      {/* Navbar */}
-      <header className="w-full fixed top-0 left-0 bg-white shadow-md z-50 h-16 md:h-20">
-        <div className="container mx-auto px-4 md:px-6 flex justify-between items-center h-full space-x-4">
-          {/* Logo and Menu Icon */}
-          <div className="flex items-center space-x-4">
-            {/* Menu Icon */}
-            <button
-              onClick={toggleSidebar}
-              className="focus:outline-none md:hidden"
-            >
-              <Image
-                src="/assets/Class/menu.png"
-                alt="Menu"
-                width={20}
-                height={20}
-              />
-            </button>
+   <>
+   {showNav &&  <div className="fixed w-full px-6 bg-white border-gray-1">
+      <nav className="flex justify-between items-center py-4">
+        <Link href={"/"} className="text-lg font-bold flex items-center rubik-mono-one-regular gap-2">
+          <Image src="/assets/chemistry-logo.png" alt="Logo" width={50} height={50} className="" />
+          <Image src="/assets/chemistry-wordmark.png" alt="Logo" width={150} height={50} className="" />
+        </Link>
+        <div className="flex space-x-4">
+          <Link href={'/profile'}>
+            <Image src="/assets/icon/user.svg" alt='User' width={35} height={35} className=""  />
+            </Link>
 
-            {/* Logo (Responsive Changes) */}
-            <div
-              className="flex-shrink-0 cursor-pointer"
-              onClick={() => handleNavigation("/homepage")}
-            >
-              {/* Use different logos based on screen size */}
-              <Image
-                src="/assets/Class/iconstelkbook.png"
-                alt="Logo Small"
-                width={50}
-                height={50}
-                className="block md:hidden"
-              />
-              <Image
-                src="/assets/Class/Stelk_bookTitle.png"
-                alt="Logo Full"
-                width={148}
-                height={88}
-                className="w-28 md:w-40 hidden md:block"
-              />
-            </div>
-          </div>
-
-          {/* Search Bar */}
-          <div className="flex-grow flex justify-center relative">
-            <div className="w-full sm:w-3/4 md:w-1/2 lg:w-1/3 flex items-center bg-[#F5F5F5] rounded-full px-4 py-2 shadow-sm">
-              <Image
-                src="/assets/Class/Search_icon.png"
-                alt="Search Icon"
-                width={20}
-                height={20}
-                className="mr-3"
-              />
-              <input
-                type="text"
-                placeholder="Pencarian disini"
-                className="flex-grow bg-transparent border-none text-gray-600 placeholder-gray-400 focus:outline-none focus:ring-0"
-              />
-            </div>
-          </div>
-
-          {/* Profile Icon */}
-          <div className="flex items-center space-x-4">
-            <div
-              className="flex-shrink-0 cursor-pointer"
-              onClick={() => handleNavigation("/profile")}
-            >
-              <Image
-                src="/assets/Class/icon_user.png"
-                alt="User Icon"
-                width={35}
-                height={35}
-                className="rounded-full"
-              />
-            </div>
-          </div>
+          <Link href={'/logout'}>
+          <Image src="/assets/icon/logout.svg" alt='Logout' width={35} height={35} className=""  />
+          </Link>
         </div>
-      </header>
-    </>
+      </nav>
+    </div>}
+    {children}
+   </>
   );
 };
 
-export default Navbar;
+export default NavbarLayout;
